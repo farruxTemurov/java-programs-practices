59c8d4418523d4f9f77e9ce05f5a006b130fd8b6
Farrux Temurov <tfarrux70@gmail.com>
2025-01-10 22:41:30 +0500

    4 options added to test preparedStatement

--- JdbcTestingApp/src/com/PreparedStatementExample.java
+++ JdbcTestingApp/src/com/PreparedStatementExample.java
@@ -3,6 +3,7 @@ package com;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.Scanner;
 
@@ -12,31 +13,85 @@ public class PreparedStatementExample {
 		Scanner sc = new Scanner(System.in);
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			System.out.println("Driver loaded successfully");
 			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "Farrux2006");
-			System.out.println("Connected successfully");
 			Statement stmt = con.createStatement();
 
-			// insert Query
+			// insert query
+			System.out.println("1: Add Emmployee 2: Delete Employee 3: Update Employee 4: Retrieve Employee");
+			System.out.println("plz enter your choice");
+			int choice = sc.nextInt();
 
-			PreparedStatement pstmt = con.prepareStatement("insert into employee values(?, ?, ?)");
-			System.out.println("Enter the id ");
-			int id = sc.nextInt();
-			pstmt.setInt(1, id);
+			if (choice == 1) {
 
-			System.out.println("Enter teh name ");
-			String name = sc.next();
-			pstmt.setString(2, name);
+				PreparedStatement pstmt = con.prepareStatement("insert into employee values(?,?,?)");
+				System.out.println("Enter the id");
+				int id = sc.nextInt();
+				pstmt.setInt(1, id); // here we set id value to 1st ? in query
 
-			System.out.println("Enter the salary ");
-			float salary = sc.nextFloat();
-			pstmt.setFloat(3, salary);
+				System.out.println("Enter the name");
+				String name = sc.next();
+				pstmt.setString(2, name); // here we set name value to 2nd ? in query
 
-			int result = pstmt.executeUpdate();
-			if (result > 0) {
-				System.out.println("Record inserted successfully!");
+				System.out.println("Enter the salary");
+				float salary = sc.nextFloat();
+				pstmt.setFloat(3, salary); // here we set salary value to 3rd ? in query
+
+				int result = pstmt.executeUpdate();
+				if (result > 0) {
+					System.out.println("Record inserted successfully");
+				}
+
+			} else if (choice == 2) {
+
+				PreparedStatement pstmt = con.prepareStatement("delete from employee where eid = ?");
+				System.out.println("Enter the id");
+				int id = sc.nextInt();
+				pstmt.setInt(1, id); // here we set id value to 1st ? in query
+
+				int result = pstmt.executeUpdate();
+				if (result > 0) {
+					System.out.println("Record deleted successfully");
+				} else {
+					System.out.println("Record not present");
+				}
+
+			} else if (choice == 3) {
+
+				PreparedStatement pstmt = con.prepareStatement("update employee set salary = ? where eid=?");
+				System.out.println("Enter the id");
+				int id = sc.nextInt();
+				pstmt.setInt(2, id); // here we set id value to 2nd ? in query
+
+				System.out.println("Enter the salary");
+				float salary = sc.nextFloat();
+				pstmt.setFloat(1, salary); // here we set salary value to 1st ? in query
+
+				int result = pstmt.executeUpdate();
+				if (result > 0) {
+					System.out.println("Record updated successfully");
+				} else {
+					System.out.println("Record not present");
+				}
+
+			} else if (choice == 4) {
+
+				PreparedStatement pstmt = con.prepareStatement("select * from employee where salary > ?");
+
+				System.out.println("Records that hold more salary than you asked will be presented!");
+				System.out.println("Enter the salary");
+				float salary = sc.nextFloat();
+				pstmt.setFloat(1, salary); // here we set salary value to 1st ? in query
+
+				ResultSet rs = pstmt.executeQuery();
+				while (rs.next()) {
+					System.out.println("Id is " + rs.getInt("eid") + " Name is " + rs.getString("ename") + " Salary is "
+							+ rs.getFloat("salary"));
+				}
+
+			} else {
+				System.out.println("wrong choice");
 			}
-			
+
 		} catch (Exception e) {
 			System.out.println(e.toString());
 		}
