dc83af7511c965f7413a2ca4fa469f73a0153bc6
Farrux Temurov <tfarrux70@gmail.com>
2025-02-01 17:46:15 +0500

    methods execution with non-static method

--- CoreJavaPractices/src/com/Methods.java
+++ CoreJavaPractices/src/com/Methods.java
@@ -14,14 +14,27 @@ public class Methods {
 		return max;
 	}
 
+	// static method | executed by class name
+	static int getMaxFromArray(int[] array) {
+		int max = array[0];
+		for (int idx = 1; idx < array.length; idx++) {
+			if (array[idx] > max) {
+				max = array[idx];
+			}
+		}
+		return max;
+	}
+
 	public static void main(String[] args) {
 		int[] productPrices = { 1200, 2100, 2400, 1294, 1099 };
 		int[] cricketerScores = { 12, 32, 40, 76, 71, 14 };
 		int[] covidCases = { 74134, 24223, 24802, 284112 };
-		
-		
-		// object construction statement 
-		
+
+		// object construction statement
+		Methods ms = new Methods();
+		System.out.println("Max in productPrices is " + ms.getMax(productPrices));
+		System.out.println("Max in productPrices is " + ms.getMax(cricketerScores));
+		System.out.println("Max in productPrices is " + ms.getMax(covidCases));
 	}
 
 }
